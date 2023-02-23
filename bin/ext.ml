0e0ab9ae6c57031fac74911b51c9836469eaef4e
Guillaume Huysmans <ghuysmans99@gmail.com>
2023-02-23 04:44:03 +0100

    Extract links to files hosted by Google

---
+++ bin/ext.ml
@@ -0,0 +1,22 @@
+open Soup
+
+let extract html =
+  let soup = parse html in
+  let show url =
+    let prefix = "https://codejam.googleapis.com/dashboard/get_file/" in
+    if String.starts_with ~prefix url then
+      print_endline url
+  in
+  select "a" soup |> iter (fun x -> show (R.attribute "href" x));
+  select "img" soup |> iter (fun x -> show (R.attribute "src" x))
+
+let () =
+  let y = Yojson.Safe.from_channel stdin in
+  match Gcomp.Dashboard.of_yojson y with
+  | Error e -> prerr_endline e
+  | Ok {challenge = {recap; tasks; _}; _} ->
+    extract recap;
+    tasks |> List.iter (fun {Gcomp.Dashboard.analysis; statement; _} ->
+      extract analysis;
+      extract statement
+    )
